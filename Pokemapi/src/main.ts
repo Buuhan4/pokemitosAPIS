97aaf9fc2e3784375613baec073bd08c4ee50cfe
Daniel <167441050+Buuhan4@users.noreply.github.com>
2025-01-13 07:05:20 +0100

    Update main.ts

--- Pokemapi/src/main.ts
+++ Pokemapi/src/main.ts
@@ -1,18 +1,35 @@
 import { Pokemon, Result } from './pokemon-interface';
 
+const pokeApi: string = "https://pokeapi.co/api/v2/pokemon?limit=151&offset=0";
 
-const pokeApi: string = "https://pokeapi.co/api/v2/pokemon/",
-  $pokeCard: HTMLElement = <HTMLElement>document.getElementById("poke-card"),
-  fragment: Node = document.createDocumentFragment();
+const pokemonContainer = document.querySelector('.pokemon-container');
 
 fetch(pokeApi)
   .then((response) => response.json())
-  .then((response: Pokemon) => {
+  .then(async (response: Pokemon) => {
+    
+    for (let index = 0; index < response.results.length; index++) {
+      const pokemon = response.results[index];
+      const imgPokemon = response;
+      const typesResponse = await fetch(pokemon.url);
+      const typesData = await typesResponse.json();
+      const types = typesData.types.map((type: any) => type.type.name).join(" ");
 
-    response.results.forEach((result) => {
-      const pokemonDiv = document.createElement("div");
-      fragment.appendChild(pokemonDiv);
-    });
+      const pokeSprite = typesData.sprites.front_default;
+      console.log(pokeSprite);
 
-    $pokeCard.appendChild(fragment);
-  });
+      const pokeCard = document.createElement('div');
+      pokeCard.classList.add('poke-card');
+
+      const capitalizeFirstLetter = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
+      const capitalizedPokemonName = capitalizeFirstLetter(pokemon.name);
+
+      pokeCard.innerHTML = `
+        <h2>${capitalizedPokemonName}</h2>
+        <img src="${pokeSprite}" alt="${capitalizedPokemonName}">
+        <p>Tipo: ${types}</p>
+      `;
+      pokemonContainer?.appendChild(pokeCard);
+    }
+  })
+  .catch((error) => console.error("Error fetching data:", error));
